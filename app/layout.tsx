9555b3673f35bdcb2eba70909275764657b1b1f8
istiak.shanto <55758239+istiak2426@users.noreply.github.com>
2026-04-30 22:37:25 +0600

    Refactor RootLayout function for better readability

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-
-
-
-
 import { SessionProvider } from "next-auth/react";
 import "./globals.css";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
